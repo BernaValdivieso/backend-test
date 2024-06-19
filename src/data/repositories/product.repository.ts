2a8485883b57dcf2b7133c7b78f172e0a43529f0
Bernardo <bavaldivieso@uc.cl>
2024-06-18 22:13:42 -0400

    feat: create product repository

---
+++ src/data/repositories/product.repository.ts
@@ -0,0 +1,79 @@
+import { Product } from "../../domain/entities/product.entity";
+import { ProductRepositoryInterface } from "../../domain/repository-interfaces/product.repository-interface";
+import AWS from "aws-sdk";
+
+const TABLE_NAME = "ProductsTable";
+
+const mapDynamoDBItemToProduct = (item: AWS.DynamoDB.DocumentClient.AttributeMap): Product => {
+  return {
+    productId: item.productId,
+    name: item.name,
+    description: item.description,
+    price: item.price,
+    isAvailable: item.isAvailable,
+  };
+};
+
+export const ProductRepository = (): ProductRepositoryInterface => ({
+  get: async (productId: string) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    const output = await docClient
+      .get({
+        TableName: TABLE_NAME,
+        Key: {
+          productId,
+        },
+      })
+      .promise();
+
+    return output.Item ? mapDynamoDBItemToProduct(output.Item) : null;
+  },
+
+  getAll: async () => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    const output = await docClient.scan({ TableName: TABLE_NAME }).promise();
+
+    return output.Items?.map(mapDynamoDBItemToProduct) ?? [];
+  },
+
+  create: async (product: Product) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: product,
+      })
+      .promise();
+
+    return product;
+  },
+
+  update: async (product: Product) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    await docClient
+      .put({
+        TableName: TABLE_NAME,
+        Item: product,
+      })
+      .promise();
+
+    return product;
+  },
+
+  delete: async (productId: string) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    await docClient
+      .delete({
+        TableName: TABLE_NAME,
+        Key: {
+          productId,
+        },
+      })
+      .promise();
+  },
+});
